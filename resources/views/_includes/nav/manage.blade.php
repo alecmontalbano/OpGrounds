96a14499ca9c6d3f5947a5869f1ac077f0789cfe
alec montalbano <alecmontalbano@alecs-MacBook-Pro.local>
2020-07-13 21:56:33 -0400

    Created Posts migration and setup PostController and routes.

--- resources/views/_includes/nav/manage.blade.php
+++ resources/views/_includes/nav/manage.blade.php
@@ -8,6 +8,20 @@
         </ul>
 
         <p class="menu-label">
+            Content
+        </p>
+        <ul class="menu-list">
+            <li><a class="{{Nav::isResource('posts')}}" href="{{route('posts.index')}}">Posts</a></li>
+            {{-- <li>
+                <a class="{{Nav::hasSegment(['roles', 'permissions'], 2)}} has-submenu">Roles &amp; Permissions</a>
+                <ul class="submenu">
+                    <li><a class="{{Nav::isResource('roles')}}" href="{{route('roles.index')}}">Roles</a></li>
+                    <li><a class="{{Nav::isResource('permissions')}}" href="{{route('permissions.index')}}">Permissions</a></li>
+                </ul>
+            </li> --}}
+        </ul>
+
+        <p class="menu-label">
             Administration
         </p>
         <ul class="menu-list">
