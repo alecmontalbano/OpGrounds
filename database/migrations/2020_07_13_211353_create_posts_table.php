1dfde0779a4b631dc121090b9121f07cc30a4352
alec montalbano <alecmontalbano@alecs-MacBook-Pro.local>
2020-07-15 21:17:43 -0400

    Added api authentication for Users and setup a UserObserver.

--- database/migrations/2020_07_13_211353_create_posts_table.php
+++ database/migrations/2020_07_13_211353_create_posts_table.php
@@ -16,7 +16,7 @@ class CreatePostsTable extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->string('slug')->unique();
-            $table->unsignedInteger('author_id');
+            $table->unsignedBigInteger('author_id');
             $table->string('title');
             $table->text('excerpt');
             $table->longText('content');
